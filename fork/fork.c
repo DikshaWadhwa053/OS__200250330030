5ce70f10af80aa37275cfbe15d9fdaf6a8da9788
Diksha <wdiksha5@gmail.com>
2020-10-16 23:08:19 +0530

    Fork program for demonstrating PID and PPID

---
+++ fork/fork.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+int main(int argc, char const *argv[])
+{   
+    pid_t id;
+    printf("Execution before fork:\n");
+    id=fork();
+        if(id==0)
+        {
+            printf("Child id:\n");
+        }
+        else
+        {
+            printf("Parents id:\n");
+        }
+        
+
+    printf("Execution after fork id\t PID\t PPID\t:%d %d %d\n",id,getpid(),getppid());
+    return 0;
+}
