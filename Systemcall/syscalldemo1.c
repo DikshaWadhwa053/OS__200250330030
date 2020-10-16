6f157ce6eb6c6898f4a7242911ee7f63efe0aee4
Diksha <wdiksha5@gmail.com>
2020-10-17 02:35:25 +0530

    System Call without O_CREAT

---
+++ Systemcall/syscalldemo1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    int fd;
+
+    fd = open("desd.txt",O_WRONLY |O_CREAT);
+
+    if(-1==fd)
+    {
+        perror("there is some error in open");
+        return -1;
+    }
+    write(fd,"cdac hyd",8);
+    close(fd);
+    return 0;
+}
