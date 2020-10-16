d630bb4f82a19a317c6361429fde8e813ad4872f
Diksha <wdiksha5@gmail.com>
2020-10-16 23:57:33 +0530

    Makefile program with multiple arithmetic funnctionalities

---
+++ BuildProcess/builddemo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int add(int, int);
+int sub(int, int);
+int mul(int,int);
+
+
+int main(int argc, char const *argv[])
+{
+    printf("the addition is:%d\n",add(10,30));
+    printf("the difference is:%d\n",sub(47,30));
+    printf("the product is:%d\n",mul(2,3));
+   
+    return 0;
+}
