406bab6b684790b1422370ed2ab4b56b15823b5d
Diksha <wdiksha5@gmail.com>
2020-10-17 13:06:08 +0530

    File Descriptors STDin,STDout,STDerror

---
+++ BufferIO/buffiodemo2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello world \n");
+    
+    return 0;
+}
