783e3846020477a213ec802be3ff455fe4ae1773
Diksha <wdiksha5@gmail.com>
2020-10-17 13:46:00 +0530

    File Descriptoors :STDin,STDout,STerror and Others

---
+++ BufferIO/buffiodemo3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    int fd;
+
+    fd = open("desd1.txt",O_WRONLY|O_CREAT);
+
+    printf("fd is:%d\n",fd);
+    if(-1==fd)
+    {
+        perror("there is some error in open");
+        return -1;
+    }
+    write(fd,"cdac hyd\n",8);
+    close(fd);
+    return 0;
+}
