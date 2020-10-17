c1bea83dbcddf09172d0e9f0e8ba5905b68fe8e1
Diksha <wdiksha5@gmail.com>
2020-10-17 11:44:48 +0530

    Difference b/w I/O and System calls

---
+++ BufferIO/buffiodemo1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello world ");
+    printf("Diksha\n");
+    printf("Wadhwa\n");
+    return 0;
+}
