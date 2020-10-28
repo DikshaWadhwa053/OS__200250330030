5219a07b9591a2da3688592b1a4b388b6eda1125
Diksha <wdiksha5@gmail.com>
2020-10-28 22:51:29 +0530

    Inheritance:if parent exit first

---
+++ inheritparent/inheritdemo.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int main(int argc, char const *argv[])
+{
+    pid_t id;
+
+    id=fork();
+    if(id==0)
+    {
+        //Child
+        printf("Child Started\n");
+        sleep(12);
+        printf("Child Exiting\n");
+    }
+    else
+    {
+    
+        //parent
+        printf("Parent starting\n");
+        sleep(5);
+        printf("Parent Exiting\n");
+    }
+    
+    return 0;
+}
