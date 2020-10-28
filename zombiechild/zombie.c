0333412ce547e8ebdd6703a8ffd4d4f2ddb76d4b
Diksha <wdiksha5@gmail.com>
2020-10-28 22:38:53 +0530

    Zombie Process:Child Exited first

---
+++ zombiechild/zombie.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int main(int argc, char const *argv[])
+{
+    pid_t id;
+
+    id=fork();
+    if(id==0)
+    {
+        //Child
+        printf("Child Started\n");
+        sleep(2);
+        printf("Child Exiting\n");
+    }
+    else
+    {
+    
+        //parent
+        printf("Parent starting\n");
+        sleep(10);
+        printf("Parent Exiting\n");
+    }
+    
+    return 0;
+}
